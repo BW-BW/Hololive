61ddde602bcba8093127f2a9c18d2e2cdb873113
BW-BW <brianwidjaja53@gmail.com>
2023-10-05 13:34:18 +0800

    Admin Login

---
+++ Hololive/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hololive.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //login function
+        public IActionResult AdminHome(string username, string password)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                // Successful login
+                return View(); ; // Redirect to AdminHome action
+            }
+            else
+            {
+                // Failed login
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View("Index"); // Replace "YourLoginView" with your actual login view name
+            }
+            //return View();
+        }
+    }
+}
